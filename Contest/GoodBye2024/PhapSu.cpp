7c46d57d7f94fe5328780d3c769f1a6f0c6a522c
phucvip123 <113783304+phucvip123@users.noreply.github.com>
2025-01-18 14:23:40 +0700

    Solve PhapSu

---
+++ Contest/GoodBye2024/PhapSu.cpp
@@ -0,0 +1,48 @@
+/*
+    Author: Nguyễn Văn Phúc
+    File name: PhapSu
+    Created at: 2025-01-18 14:13
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define F first
+#define S second
+#define pb push_back
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define di deque<int>
+#define endl '\n'
+#define sz(x) ((int)x.size())
+#define all(p) p.begin(), p.end()
+#define bug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+#define print(a)          \
+    for (auto x : a)      \
+        cout << x << " "; \
+    cout << endl
+#define print1(a)    \
+    for (auto x : a) \
+        cout << x.F << " " << x.S << endl
+#define print2(a, x, y)         \
+    for (int i = x; i < y; i++) \
+        cout << a[i] << " ";    \
+    cout << endl
+#define no "NO\n"
+#define yes "YES\n"
+
+main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    int n; cin >> n;
+    vi a(n);
+    for(int i=0;i<n;i++) cin >> a[i]; // for(int &i: a) cin >> i; i <=> a[i]
+    int sum = 0;
+    for(int i=n-1;i>=0;i--){
+        if(i < n-1 && a[i] > a[i + 1]) a[i] = a[i+1];
+        sum += a[i];
+    }
+    cout << sum << endl;
+}
